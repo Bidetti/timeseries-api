bc461a9d0dd3f852bd047e552cde6cb8a7be7425
Rafael Bidetti <rafael.bidetti@gmail.com>
2024-05-15 20:33:58 -0300

    Fix: Env Reader

--- database/database.go
+++ database/database.go
@@ -3,10 +3,18 @@ package database
 import (
 	"log"
 	"os"
+
 	"github.com/InfluxCommunity/influxdb3-go/influxdb3"
+	"github.com/joho/godotenv"
 )
 
 func ConnectToDB() (*influxdb3.Client, error) {
+	// Carrega o arquivo .env que está um diretório acima
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Erro carregando o arquivo .env")
+	}
+
 	url := os.Getenv("INFLUXDB_URL")
 	token := os.Getenv("INFLUXDB_TOKEN")
 	database := os.Getenv("INFLUXDB_DATABASE")
@@ -19,8 +27,8 @@ func ConnectToDB() (*influxdb3.Client, error) {
 
 	if err != nil {
 		log.Fatal("Failed to connect to database")
-		return &influxdb3.Client{}, err
-	} 
+		return nil, err
+	}
 
 	return influxdb3Client, nil
 }
